fb41d46f8ca011c015c8a0fc70c72be277f886e8
RamizGindullin-UU <ramiz.gindullin@it.uu.se>
2025-09-02 11:06:01 +0200

    fixed the comments

--- utility.py
+++ utility.py
@@ -1,24 +1,24 @@
-% Copyright 2025 Ramiz Gindullin.
-%
-% Licensed under the Apache License, Version 2.0 (the "License");
-% you may not use this file except in compliance with the License.
-% You may obtain a copy of the License at
-%
-%     http://www.apache.org/licenses/LICENSE-2.0
-%
-% Unless required by applicable law or agreed to in writing, software
-% distributed under the License is distributed on an "AS IS" BASIS,
-% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-% See the License for the specific language governing permissions and
-% limitations under the License.
-%
-%
-% Description:  Various functions
-%
-% Authors: Ramiz GINDULLIN (ramiz.gindullin@it.uu.se)
-% Version: 1.0
-% Last Revision: September 2025
-%
+# Copyright 2025 Ramiz Gindullin.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+# Description:  A simple GUI for MiniZinc microplates models
+#
+# Authors: Ramiz GINDULLIN (ramiz.gindullin@it.uu.se)
+# Version: 1.0
+# Last Revision: September 2025
+#
 
 
 import os
